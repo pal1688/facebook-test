b2b75c75b2a112d5b5c684981bf3e0e1c2e63534
Pallavi Bawaskar <pal@Pallavis-MacBook-Air.local>
2018-02-18 17:10:26 -0800

    adding more tests

--- src/com/Facebook/HomePage.java
+++ src/com/Facebook/HomePage.java
@@ -1,5 +1,7 @@
 package com.Facebook;
 
+import java.sql.Driver;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,37 +15,84 @@ public class HomePage {
 	@Test
 	public void Login() throws InterruptedException {
 
-	
+
 		System.setProperty("webdriver.chrome.driver","/Users/pal/Downloads/chromedriver");
 		WebDriver driver = new ChromeDriver();
+		driver.get("facebook.com");
+
+		driver.findElement(By.id("email")).sendKeys("swapal1982@gmail.com");
+
+
+		WebElement elePass = driver.findElement(By.id("pass"));
+		elePass.click();
+		elePass.sendKeys("Silverspoon@20008");
+
+		driver.findElement(By.xpath("//input[(@value='Log In')")).click();
+
+
+
+		Thread.sleep(20000);
+		driver.findElement(By.xpath("//a/span[contains(text(),'Swappal'"));  //CHECK XPATH
+		driver.findElement(By.xpath("//div[contains text(),'Swappal Binakar']"));//chekck
+
+
+		Assert.assertEquals(driver.getTitle(),"Swappal Binakar"); 
+
+		Assert.assertEquals("https://www.facebook.com/swappal.binakar.7", driver.getCurrentUrl());
+
+	}
+     
+	@Test
+	public void invalidLogin() {
+		
+		System.setProperty("WebDriver.chrome.driver", "/Users/pal/Downloads/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
 	    driver.get("facebook.com");
 	    
-	    driver.findElement(By.id("email")).sendKeys("swapal1982@gmail.com");
+	    driver.findElement(By.id("email")).sendKeys("pala");
 	    
+	    driver.findElement(By.id("pass")).sendKeys("asads");
 	    
+	    driver.findElement(By.xpath("//input[@value='Log In']")).click();
 	    
-	   
-	    
-	   WebElement elePass = driver.findElement(By.id("pass"));
-	   elePass.click();
-	   elePass.sendKeys("Silverspoon@20008");
-	   
-	    driver.findElement(By.xpath("//input[(@value='Log In')")).click();
-	    
-	
-	    
-	    Thread.sleep(20000);
-		driver.findElement(By.xpath("//a/span[contains(text(),'Swappal'"));  //CHECK XPATH
-	    driver.findElement(By.xpath("//div[contains text(),'Swappal Binakar']"));//chekck
-	    
-	 
-	    Assert.assertEquals("Swappal Binakar", driver.getTitle()); 
-	    
-	    Assert.assertEquals("https://www.facebook.com/swappal.binakar.7", driver.getCurrentUrl());
-	   
-	    
-	    
+	    driver.findElement(By.xpath("//div[contains text(), 'The email or phone number you’ve entered doesn’t match any account.']"));
+		
+		Assert.assertEquals(driver.getTitle(), "Log into Facebook | Facebook");
+		Assert.assertEquals(driver.getCurrentUrl(), "view-source:https://www.facebook.com/login.php?login_attempt=1&lwv=110 ");
+		
+		
+		
+		
 	}
+	
+	
+	public void pwdReset() {
+		
+			
+			System.setProperty("WebDriver.chrome.driver", "/Users/pal/Downloads/chromedriver");
+			WebDriver driver = new ChromeDriver();
+			
+		    driver.get("facebook.com");
+		    
+		    driver.findElement(By.id("email")).sendKeys("swapal1982");
+		    
+		    driver.findElement(By.id("pass")).sendKeys("asads");
+		    
+		    driver.findElement(By.xpath("//input[@value='Log In']")).click();
+		    
+		    driver.findElement(By.xpath("//div/div[contains text(),'Forgot your password?']");
+		    
+		    
+		
+		
+		
+		
+	}
+	
+	
+	
+	
 	
 
 }
--- src/com/Facebook/InvalidUsername.java
+++
@@ -1,5 +0,0 @@
-package com.Facebook;
-
-public class InvalidUsername {
-
-}
